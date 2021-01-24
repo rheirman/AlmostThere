d42f76ac9cca11c937eb35c45f5ccef5543b694d
rheirman <roelheirman@gmail.com>
2021-01-24 12:59:47 +0100

    initial commit: setup base project

---
+++ 1.2/Source/AlmostThere/Base.cs
@@ -0,0 +1,37 @@
+﻿using HugsLib;
+using HugsLib.Utils;
+using SearchAndDestroy.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace SearchAndDestroy
+{
+    class Base : ModBase
+    {
+        public override string ModIdentifier => "AlmostThere";
+        public static Base Instance { get; private set; }
+        private ExtendedDataStorage _extendedDataStorage;
+
+        public Base()
+        {
+            Instance = this;
+        }
+        public ExtendedDataStorage GetExtendedDataStorage()
+        {
+            return _extendedDataStorage;
+        }
+        public override void WorldLoaded()
+        {
+            if (_extendedDataStorage == null)
+            {
+                _extendedDataStorage = Find.World.GetComponent<ExtendedDataStorage>();
+            }
+            base.WorldLoaded();
+        }
+    }
+
+
+}
---
+++ 1.2/Source/AlmostThere/Storage/ExtendedCaravanData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+//Note: Currently this class contains information specific for other mods (caravanMount, caravanRider, etc), which is of course not ideal for a core framework. Ideally it should be completely generic. However I have yet to come up with an
+// way to do this properly without introducing a lot of extra work. So for now I'll just keep it as it is. 
+
+namespace SearchAndDestroy.Storage
+{
+    public class ExtendedCaravanData : IExposable
+    {
+        public bool SD_enabled = false;
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref SD_enabled, "SD_enabled", false);            
+        }
+    }
+}
